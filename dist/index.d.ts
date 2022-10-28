481caba0f418657b0b363d8dd2e62a8d70b19a19
Doug A <douganderson444@gmail.com>
2022-10-28 12:18:33 -0300

    install, build, export module src/index

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import GQLResultInterface, { GQLEdgeInterface, GQLNodeInterface } from "./faces";
+export declare const setEndpointUrl: (full_GQL_Url: string) => string;
+export declare const run: (query: string, variables?: Record<string, unknown> | undefined) => Promise<GQLResultInterface>;
+export declare const all: (query: string, variables?: Record<string, unknown> | undefined) => Promise<GQLEdgeInterface[]>;
+export declare const tx: (id: string) => Promise<GQLNodeInterface>;
+export declare const fetchTxTag: (id: string, name: string) => Promise<string | undefined>;
